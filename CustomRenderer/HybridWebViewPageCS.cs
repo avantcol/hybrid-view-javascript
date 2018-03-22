d4f412d3610615cbcf62eb2c301551787b37be04
Andrei Tatarinov <andrei@iMac.local>
2018-03-23 00:10:06 +0700

    javascript

---
+++ CustomRenderer/HybridWebViewPageCS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CustomRenderer
+{
+	public class HybridWebViewPageCS : ContentPage
+	{
+		public HybridWebViewPageCS ()
+		{
+			var hybridWebView = new HybridWebView {
+				//Uri = "index.html",
+				Uri = "https://avantrack.co/index.jsp",
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+
+			hybridWebView.RegisterAction (data => DisplayAlert ("Alert", "Hello " + data, "OK"));
+
+			Padding = new Thickness (0, 20, 0, 0);
+			Content = hybridWebView;
+		}
+	}
+}
